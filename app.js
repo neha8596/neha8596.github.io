a8cda348d9e5610992a75ff376c92317408e2f08
unknown <nehajmnaik@gmail.com>
2020-05-18 20:41:47 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,31 @@
+var express =require("express");
+var app=express();
+
+var bodyParser=require("body-parser");
+app.set("view engine","ejs");
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/",function(req,res){
+	res.render("home");
+});
+
+app.get("/education",function(req,res){
+	res.render("education");
+});
+
+app.get("/projects",function(req,res){
+	res.render("projects");
+});
+
+app.get("/experience",function(req,res){
+	res.render("experience");
+});
+
+app.get("/contact",function(req,res){
+	res.render("contact");
+});
+
+app.listen(3000,function(){
+	console.log("Server listening on port 3000");
+});
