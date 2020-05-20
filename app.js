467e697fd6be5e90a344ea2b944914e1a61982d7
unknown <nehajmnaik@gmail.com>
2020-05-20 21:29:45 +0530

    app.js listener port updated

--- app.js
+++ app.js
@@ -26,6 +26,6 @@ app.get("/contact",function(req,res){
 	res.render("contact");
 });
 
-app.listen(3000,function(){
+app.listen(process.env.PORT||3000,function(){
 	console.log("Server listening on port 3000");
 });
