cb0854b5da492e1721a5835cadd595cdcc62a4a2
unknown <nehajmnaik@gmail.com>
2020-05-20 14:41:48 +0530

    navbar highlighting added and finished experience page

---
+++ public/js/index.js
@@ -0,0 +1,24 @@
+// $( "#ulfont a" ).on( "click", function () {
+// 	$( "#ulfont").find( "li.highlight" ).removeClass( 'highlight' );
+//     $( this ).parent("li").addClass( 'highlight' );
+// });
+
+// $(document).ready(function() {
+//         $("#ulfont li").click(function(){
+//             $(this).addClass("highlight");
+//             $(this).siblings().removeClass("highlight");
+
+//         });
+//     });
+
+var setDefaultActive = function() {
+    var path = window.location.pathname;
+    var pathnew= path.substr(1);
+    var element = $("#ulfont a[href='" + pathnew + "']");
+	element.parent("li").siblings().removeClass("highlight")
+    element.parent("li").addClass("highlight");
+}
+
+$(document).ready(function() {
+	setDefaultActive();
+});
