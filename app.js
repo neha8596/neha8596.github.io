e0b64af77e4abf25be3728ad500de620fc621140
unknown <nehajmnaik@gmail.com>
2020-05-21 10:36:18 +0530

    app.js modified

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 var express =require("express");
 var app=express();
 
-var bodyParser=require("body-parser");
 app.set("view engine","ejs");
 app.use(express.static('public'));
-app.use(bodyParser.urlencoded({extended:true}));
+
 
 app.get("/",function(req,res){
 	res.render("home");
@@ -26,6 +25,8 @@ app.get("/contact",function(req,res){
 	res.render("contact");
 });
 
+
+//URL of the application :- https://infinite-lowlands-88093.herokuapp.com/
 app.listen(process.env.PORT||3000,function(){
 	console.log("Server listening on port 3000");
 });
